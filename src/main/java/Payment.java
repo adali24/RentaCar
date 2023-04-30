0cf6622941be0aac3bd97ae68c853480fa357d6c
Hamit <sadecehamit@gmail.com>
2023-04-30 22:36:21 +0200

    test2

--- src/main/java/Payment.java
+++ src/main/java/Payment.java
@@ -3,7 +3,7 @@ public class Payment {
        -	OdemeBilgieri() metodu ile odeme bilgilerini alacaginiz bir method create ediniz.
         Kart numarası uzunluğu 12 olmalıdır, kontrol ediniz.
 
- //git push -u origingit push -u origin //git push -u origingit push -u origin
+
 
      */
 }
