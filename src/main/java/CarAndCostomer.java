8caa5c9fc453dec6df7d83539a5ed461a5da3a4a
Hamit <sadecehamit@gmail.com>
2023-04-30 22:33:11 +0200

    test

---
+++ src/main/java/CarAndCostomer.java
@@ -0,0 +1,11 @@
+public class CarAndCostomer {
+    /*
+    Make,(Marka)
+    Model, (Model)
+    Fuel Type,(Yakit Tipi)
+    Gear and (Vites)
+    Daily Fare (vehicle related information).(Gunluk ucret)
+
+     */
+
+}
---
+++ src/main/java/Payment.java
@@ -0,0 +1,9 @@
+public class Payment {
+    /*
+       -	OdemeBilgieri() metodu ile odeme bilgilerini alacaginiz bir method create ediniz.
+        Kart numarası uzunluğu 12 olmalıdır, kontrol ediniz.
+
+ //git push -u origingit push -u origin //git push -u origingit push -u origin
+
+     */
+}
