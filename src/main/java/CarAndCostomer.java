236e09b97317f492f1c9d117b60adf0a3c16b967
Zulfiye <zulfiyezulfiyek@gmail.com>
2023-05-03 02:18:43 +0300

    degisiklikler

--- src/main/java/CarAndCostomer.java
+++ src/main/java/CarAndCostomer.java
@@ -8,6 +8,7 @@ public class CarAndCostomer {
 
      */
 
-
+//github da branch master'da iken yapilan degisikliklerin de master dan push edilmesi ile degisiklikler aninda gorunuyor.
+// simdi ise hem github'da hem lokalde branch'i kendi branch'ima aldim ve lokalde eklemeler yapacagim .
 
 }
