a3af71e5b57f91cde92a81737848accc51ada870
Hamit <48844072+adali24@users.noreply.github.com>
2023-05-03 00:16:35 +0200

    Update CarAndCostomer.java

--- src/main/java/CarAndCostomer.java
+++ src/main/java/CarAndCostomer.java
@@ -8,14 +8,4 @@ public class CarAndCostomer {
 
      */
 
-
-    //degisiklikler yapildimi?
-    
-    //yeni kodlar eklendi
-    //omer yeni kodlari ekledi.
-    //fdjfalkjf
-
-
-//yeni baharlar gelecek
-
 }
