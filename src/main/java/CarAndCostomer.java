1902ab5ec91b8228b4deecbfc8b62f1431453414
Zulfiye <zulfiyezulfiyek@gmail.com>
2023-05-03 02:35:32 +0300

    Z3

--- src/main/java/CarAndCostomer.java
+++ src/main/java/CarAndCostomer.java
@@ -9,5 +9,10 @@ public class CarAndCostomer {
     Daily Fare (vehicle related information).(Gunluk ucret)
 
      */
+
+
+
+    //github da branch master'da iken yapilan degisikliklerin de master dan push edilmesi ile degisiklikler aninda gorunuyor.
+// simdi ise hem github'da hem lokalde branch'i kendi branch'ima aldim ve lokalde eklemeler yapmayi deneyecegim. .
 }
 
