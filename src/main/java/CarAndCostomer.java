e297d2438638067b5f1648d40deedcfa08d79728
Fatma <karacalifatma80@gmail.com>
2023-05-11 00:26:43 +0300

    first commit

---
+++ src/main/java/CarAndCostomer.java
@@ -0,0 +1,35 @@
+package main.java;
+
+public class CarAndCostomer {
+    /*
+    Make,(Marka)
+    Model, (Model)
+    Fuel Type,(Yakit Tipi)
+    Gear and (Vites)
+    Daily Fare (vehicle related information).(Gunluk ucret)
+
+     */
+
+
+
+
+
+//omer yeni kodlar ekledi
+    //omer yeni kodlar eklemeye devam ediyor
+
+
+
+
+//github da branch master'da iken yapilan degisikliklerin de master dan push edilmesi ile degisiklikler aninda gorunuyor.
+// simdi ise hem github'da hem lokalde branch'i kendi branch'ima aldim ve lokalde eklemeler yapmayi deneyecegim. .
+
+    // ayni islemi tekrarlamayi deneyecegim.
+    // tekrar deneyecegim..
+    // tekrar tekrar yazmayi deniyorum.
+
+}
+
+
+
+//confliks yapacagiz
+
---
+++ src/main/java/Payment.java
@@ -0,0 +1,14 @@
+
+public class Payment {
+    /*
+       -	OdemeBilgieri() metodu ile odeme bilgilerini alacaginiz bir method create ediniz.
+        Kart numarası uzunluğu 12 olmalıdır, kontrol ediniz.
+
+
+
+     */
+
+//hamit git hub icin deneme satiri
+
+    // deniyoruz
+}
